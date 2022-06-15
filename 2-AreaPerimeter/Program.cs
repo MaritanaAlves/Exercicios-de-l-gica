46541f26bc3b0be09431c14f6708e6b353b89393
Maritana Alves <maritana.cz@gmail.com>
2022-06-15 20:23:27 -0300

    added exercises

---
+++ 2-AreaPerimeter/Program.cs
@@ -0,0 +1,14 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        //* Escreva um programa que leia o valor do lado de um quadrado e calcule a sua área e o seu perímetro.*//
+        Console.Write("Digite o lado do quadrado: ");
+        double lado = double.Parse(Console.ReadLine());
+
+        double area = lado * lado;
+        double perimetro = 4 * lado;
+
+        Console.Write("A área do quadrado é: " + area + "m², o perímetro é: " + perimetro + "m.");
+    }
+}
