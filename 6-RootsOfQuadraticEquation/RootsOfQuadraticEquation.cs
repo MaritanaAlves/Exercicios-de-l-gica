398d37362f8b5960a998d1110a9e3231a1cd943e
Maritana Alves <maritana.cz@gmail.com>
2023-10-06 15:24:02 -0300

    exercise 6 finished

--- 6-RootsOfQuadraticEquation/RootsOfQuadraticEquation.cs
+++ 6-RootsOfQuadraticEquation/RootsOfQuadraticEquation.cs
@@ -1,42 +1,49 @@
 ﻿public class RootsOfQuadraticEquation
 {
+    double x1;
+    double x2;
+
     static void Main (string[] args)
     {
         //Ler os valores dos três coeficientes "a", "b" e "c" de uma equação do segundo grau ( +  +  = 0)
-        //Em seguida, mostrar os valores das raízes da equação, conforme exemplos, usando a fórmula de Baskara(veja
-        //abaixo).Se a equação não possuir raízes(o valor de "a" não pode ser zero, e o valor de "delta" não pode ser
+        //Em seguida, mostrar os valores das raízes da equação, usando a fórmula de Baskara.
+        //Se a equação não possuir raízes(o valor de "a" não pode ser zero, e o valor de "delta" não pode ser
         //negativo), mostrar uma mensagem "Impossivel calcular".
 
+        Console.WriteLine("Enter with 'a' value:");
         int a = int.Parse(Console.ReadLine());
+        Console.WriteLine("Enter with 'b' value:");
         int b = int.Parse(Console.ReadLine());
+        Console.WriteLine("Enter with 'c' value:");
         int c = int.Parse(Console.ReadLine());
 
         var rootsOfQuadraticEquation = new RootsOfQuadraticEquation();
-        
         int delta = rootsOfQuadraticEquation.CalculateDelta(a, b, c);
-        double x1 = (- b + Math.Sqrt(delta)) / (2 * a);
-        double x2 = (- c - Math.Sqrt(delta)) / (2 * b);
+        string roots = rootsOfQuadraticEquation.CalculateRootsOfQuadraticEquation(a, b, c, delta);
 
+        Console.WriteLine(roots);
     }
+
     public int CalculateDelta(int a, int b, int c)
     {
         return (b * b) - (4 * a * c);
     }
-    public string CalculateRootsOfQuadraticEquation(int a, int b, int c, int delta, double x1, double x2)
+
+    public string CalculateRootsOfQuadraticEquation(int a, int b, int c, int delta)
     {
         if (a == 0)
         {
-            return "The value a can't be zero";
+            return "The value of 'a' can't be zero";
 
         }
         if (delta < 0)
         {
             return "The value of delta can't be negative";
         }
-        else
-        {
-            return
-        }
-    }
+      
+        x1 = (-b + Math.Sqrt(delta)) / (2 * a);
+        x2 = (-b - Math.Sqrt(delta)) / (2 * b);
 
+        return $"The roots of equation is x1: {x1} and x2: {x2}"; 
+    }
 }
